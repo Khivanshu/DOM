2f6aeed6e771cc0693ce2292a8ea19d25c9c9d1e
Khivanshu <124436550+Khivanshu@users.noreply.github.com>
2023-05-11 21:16:43 +0530

    Add files via upload

---
+++ 03 Mouseover/script.js
@@ -0,0 +1,8 @@
+const para = document.getElementById("para");
+const clickbtn = document.getElementById("clickbtm");
+
+
+
+clickbtn.addEventListener("mouseover",function (){
+    alert("your mouse enter in restricted area");
+});
