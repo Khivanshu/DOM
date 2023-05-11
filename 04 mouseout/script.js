95e1bb3122fca3b26258d0c39a167b9dd4af2ba8
Khivanshu <124436550+Khivanshu@users.noreply.github.com>
2023-05-11 21:17:05 +0530

    Add files via upload

---
+++ 04 mouseout/script.js
@@ -0,0 +1,8 @@
+
+const clickbtn = document.getElementById("clickbtm");
+
+
+clickbtn.addEventListener("mouseout",function () {
+    document.getElementById("para").innerText = "Plz don't go";
+
+});
