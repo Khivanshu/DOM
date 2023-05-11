9f6c74077a07723ff64b073950819ccdc7d918d0
Khivanshu <124436550+Khivanshu@users.noreply.github.com>
2023-05-11 21:16:02 +0530

    Add files via upload

---
+++ 01 Onclick/script.js
@@ -0,0 +1,9 @@
+const para = document.getElementById("para");
+const clickbtn = document.getElementById("clickbtm");
+
+
+
+clickbtn.addEventListener("click",function (){
+    para.style.color = "red";
+    para.style.fontWeight = "900";
+});
